1c51e792d25c557711f6aafcd03b2d659f6fad1a
qerimidr <qerimidr@132.161.196.136>
2025-03-03 23:20:04 -0600

    Finished part 3 method implementations for the lab

--- src/main/java/edu/grinnell/csc207/genericslab/LinkedList.java
+++ src/main/java/edu/grinnell/csc207/genericslab/LinkedList.java
@@ -5,7 +5,7 @@ package edu.grinnell.csc207.genericslab;
  * @author Drilon and Emily
  * @param <T>
  */
-public class LinkedList<T>{
+public class LinkedList<T> {
 
     public class Node<T> {
 
@@ -79,4 +79,62 @@ public class LinkedList<T>{
             }
         }
     }
+
+    /**
+     * inserts sep in between each element of this list.
+     */
+    public void intersperse(T sep) {
+        Node<T> cur = first;
+        while (cur.next != null) {
+            Node<T> newNode = new Node<>(sep, cur.next);
+            cur.next = newNode;
+            cur = newNode.next;
+            size++;
+        }
+    }
+
+    /**
+     * This method cannot be implemented for a generic T because finding the
+     * maximum requires comparing elements, which we cannot do with generics.
+     *
+     * returns the maximum element found in the list.
+     *
+     */
+    public T maximum() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * This method cannot be implemented for all generic types T because we
+     * cannot compare generic T elements between each other.
+     *
+     * sorts this list using the insertion sort algorithm. Insertion sort
+     * proceeds by looping over the elements of the array, maintaining the
+     * following invariant:
+     */
+    public void insertionSort() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Implemented referring to the reading arrayToString func using
+     * StringBuilder returns a string representation of the list in the form:
+     * [x1, x2, ..., xk] where x1, x2, ..., xk are the elements of the list.
+     *
+     * @param <T>
+     * @return List as a string
+     */
+    public String ToString() {
+        StringBuilder buf = new StringBuilder();
+        buf.append('[');
+        Node<T> cur = first;
+        while (cur != null) {
+            buf.append(String.valueOf(cur.value));
+            buf.append(", ");
+            cur = cur.next;
+        }
+        buf.append(']');
+        return buf.toString();
+    }
+
 }
